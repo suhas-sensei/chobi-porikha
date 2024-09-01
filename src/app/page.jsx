7b09b849b91980c655d5274bcf05ab9a9fc30c50
Suhas Ghosal <suhas.ghosal2002@gmail.com>
2024-09-01 22:45:17 +0530

    testing

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to Next.js!</h1>
+      <img
+        src="https://media.discordapp.net/attachments/812390695864238201/1277889611342938182/image.png?ex=66cecef1&is=66cd7d71&hm=b64ddacef9191a1022028eaaed5a6fe042a75a6e8eac8998de5bfab78288743f&=&format=webp&quality=lossless"
+        className="h-60 w-20 ml-8"
+        alt="Dynamic Island"
+      />
+    </>
+  );
+}
