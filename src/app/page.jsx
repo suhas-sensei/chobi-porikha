a05e086669f59b78f4ad8f9e23e01ee6b5f34e5f
Suhas Ghosal <suhas.ghosal2002@gmail.com>
2024-09-01 22:52:14 +0530

    chobi fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <h1>Welcome to Next.js!</h1>
       <img
-        src="https://media.discordapp.net/attachments/812390695864238201/1277889611342938182/image.png?ex=66cecef1&is=66cd7d71&hm=b64ddacef9191a1022028eaaed5a6fe042a75a6e8eac8998de5bfab78288743f&=&format=webp&quality=lossless"
+        src="https://imgur.com/a/sg8imtv"
         className="h-60 w-20 ml-8"
         alt="Dynamic Island"
       />
